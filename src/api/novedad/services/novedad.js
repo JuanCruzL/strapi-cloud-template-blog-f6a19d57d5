fc6f590d4f1172f2767564737494a9faa83696ca
JuanCruzL <juanjc2002@hotmail.com>
2025-12-23 11:40:35 -0300

    created novedad content type

--- src/api/novedad/services/novedad.js
+++ src/api/novedad/services/novedad.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article service.
+ * novedad service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService('api::novedad.novedad');
